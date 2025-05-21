26c85250143704639be8489d121ee4cbddc4f940
Raquel de Sá <silva.raquel.de.sa@gmail.com>
2025-05-21 00:10:39 -0400

    fix: importacao da funcao cn

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -12,8 +12,8 @@ import {
   type FieldPath,
   type FieldValues,
 } from "react-hook-form";
+import { cn } from "@/app/lib/utils";
 
-import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 
 const Form = FormProvider;
